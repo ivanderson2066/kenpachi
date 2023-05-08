c6921cf5778c64d96dac6a2b73643f904e675aa4
ivanderson2066 <132177377+ivanderson2066@users.noreply.github.com>
2023-05-08 13:47:43 -0300

    simulador de dado.py

--- phyton teste/simulador de dado.py
+++ phyton teste/simulador de dado.py
@@ -1,28 +1,39 @@
 #simulador de ddado
 #simular um dado, gerando valor de 1 ate 6
 import random
-
-import random
+import tkinter as tk
 
 class SimuladorDeDado:
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
-        self.mensagem = 'Você gostaria de gerar um novo valor de dado? Digite "s" para sim ou "n" para não: '
 
-    def iniciar(self):
-        while True:
-            resposta = input(self.mensagem)
-            if resposta.lower() == 's' or resposta.lower() == 'sim':
-                self.gerar_valor_do_dado()
-            elif resposta.lower() == 'n' or resposta.lower() == 'não':
-                print('Agradecemos sua participação!')
-                break
-            else:
-                print('Favor digitar apenas "s" ou "n".')
-    
     def gerar_valor_do_dado(self):
-        print(random.randint(self.valor_minimo, self.valor_maximo))
+        return random.randint(self.valor_minimo, self.valor_maximo)
 
-simulador = SimuladorDeDado()
-simulador.iniciar()
+class InterfaceGrafica:
+    def __init__(self, master):
+        self.master = master
+        master.title('Simulador de Dado')
+
+        self.label = tk.Label(master, text='Clique no botão para gerar um novo valor de dado.')
+        self.label.pack()
+
+        self.button = tk.Button(master, text='Gerar Dado', command=self.gerar_dado)
+        self.button.pack()
+
+        self.resultado = tk.Label(master, text='', font=('Helvetica', 36))
+        self.resultado.pack()
+
+        self.quit_button = tk.Button(master, text='Sair', command=master.quit)
+        self.quit_button.pack()
+
+        self.simulador = SimuladorDeDado()
+
+    def gerar_dado(self):
+        resultado = self.simulador.gerar_valor_do_dado()
+        self.resultado.config(text=str(resultado))
+
+root = tk.Tk()
+interface_grafica = InterfaceGrafica(root)
+root.mainloop()
